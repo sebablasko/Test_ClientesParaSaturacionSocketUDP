03c158aa26dc7d92061e15e92ed039252fc3c650
Sebastian <sebablasko@gmail.com>
2015-08-26 12:49:55 -0300

    updated run

--- run.sh
+++ run.sh
@@ -4,39 +4,92 @@
 #	serverThreads: número de Threads que correrán en la instancia del servidor para procesar los paquetes enviados por el cliente
 #	clients: número de instancias del programa cliente que se ejecutarán para saturar el servidor
 #	repetitions: cantidad de repeticiones de la prueba
+#
+# salida=threads_sockets_relation.csv
+#
+# make all
+#
+# for serverThreads in {1..4}
+# do
+# 	linea=$serverThreads";"
+# 	for clients in {1..5}
+# 	do
+# 		echo $serverThreads" threads en servidor"
+# 		echo $clients" clientes corriendo"
+#
+# 		repetitions=3
+# 		for ((i=1 ; $i<=$repetitions ; i++))
+# 		{
+# 			./server $serverThreads >> aux &
+# 			pid=$!
+# 			sleep 1
+#
+# 			for ((j=1 ; $j<=$clients ; j++))
+# 			{
+# 				./client 127.0.0.1 > /dev/null &
+# 			}
+# 			wait $pid
+# 			linea="$linea$(cat aux)"
+# 			rm aux
+# 		}
+# 		linea="$linea;"
+# 	done
+# 	echo "$linea" >> $salida
+# done
+#
+# make clean
+#
+# python postProcessing.py $salida
 
+
+
+
+
+echo "Compilando..."
+make all
+echo "Done"
+
+MAX_PACKS=1000000
+num_port=1820
+repetitions=3
+total_num_threads_per_socket="1 2 4 8 16"
+total_clients="1 2 3 4 5 6 7"
+
+echo "Iniciando Prueba..."
 salida=threads_sockets_relation.csv
 
-make all
-
-for serverThreads in {1..4}
+for num_threads_per_socket in $total_num_threads_per_socket
 do
-	linea=$serverThreads";"
-	for clients in {1..5}
-	do
-		echo $serverThreads" threads en servidor"
-		echo $clients" clientes corriendo"
+	linea=$num_threads_per_socket"; "
 
-		repetitions=3
+	for num_clients in $total_clients
+	do
+		echo "ServerThreads: "$num_threads_per_socket", Clientes: "$num_clients
+
 		for ((i=1 ; $i<=$repetitions ; i++))
 		{
-			./server $serverThreads >> aux &
-			pid=$!
+			echo "Repeticion $i"
+
+			./serverTesis --packets $MAX_PACKS --threads $num_threads_per_socket --port $num_port >> aux &
 			sleep 1
 
-			for ((j=1 ; $j<=$clients ; j++))
+			for ((j=1; $j<=$num_clients; j++))
 			{
-				./client 127.0.0.1 > /dev/null &
+				./clientTesis --packets "$(($MAX_PACKS*10))" --ip 127.0.0.1 --port $num_port > /dev/null &
 			}
-			wait $pid
+
+			wait $(pgrep 'serverTesis')
+
 			linea="$linea$(cat aux)"
 			rm aux
 		}
 		linea="$linea;"
 	done
+
 	echo "$linea" >> $salida
+	echo ""
 done
 
 make clean
 
-python postProcessing.py $salida
+python postProcessing.py $salida $total_clients | sed 's/\./,/g' > "fix_"$salida
